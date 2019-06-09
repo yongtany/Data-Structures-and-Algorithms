005ab8e83897c76e620b15f270b90d6a3322ffff
yongtany <yongn1494@gmail.com>
2019-06-09 13:04:07 +0900

    Upper bound

---
+++ Algorithms/upper_bound.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int n, k, A[100000001];
+int solve(int s, int e)
+{
+    int m;
+    while(e-s>0)
+    {
+        m = (s+e)/2;
+        if(A[m] <= k) s=m+1;
+        else e=m;
+    }
+    return e + 1;
+}
+
+int main()
+{
+    scanf("%d", &n);
+    for(int i=0; i<n; i++)
+        scanf("%d", A+i);
+    scanf("%d", &k);
+    printf("%d\n", solve(0, n));
+    return 0;
+}
