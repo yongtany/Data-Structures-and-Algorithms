5e0b63842afcb6aec0c753bfda6f615d7f91df67
yongtany <yongn1494@gmail.com>
2019-06-09 12:09:40 +0900

    Lower_bound

---
+++ Algorithms/lower_bound.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, k, A[100001];
+
+int main()
+{
+    cin >> n;
+    for(int i=0; i<n; i++)
+        cin >> A[i];
+    cin >> k;
+    cout << lower_bound(A, A+n, k) - A+1 << endl;
+}
