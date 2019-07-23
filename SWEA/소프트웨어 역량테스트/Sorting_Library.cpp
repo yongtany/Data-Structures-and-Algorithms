0f28d5c5f1df83fc23daa4f3731c28c507a05e50
yongtany <yongn1494@gmail.com>
2019-07-23 13:23:41 +0900

    5-3-1. 딱지 수집

--- SWEA/소프트웨어 역량테스트/Sorting_Library.cpp
+++ SWEA/소프트웨어 역량테스트/Sorting_Library.cpp
@@ -45,3 +45,37 @@ int main() {
 }
 */
 
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+struct st {
+  int t1, t2;
+};
+// t1 -> t2순으로 오름차순 정렬
+bool comp1(const struct st &a, const struct st &b) {
+  if(a.t1 == b.t1) return a.t2 < b.t2;
+  else return a.t1 < b.t1;
+}
+// t1 -> t2순으로 내림차순 정렬
+bool comp2(const struct st &a, const struct st &b) {
+  if(a.t1 == b.t1) return a.t2 > b.t2;
+}
+
+int main() {
+  st arr2[10];
+  for(int i = 0; i < 10; i++) {
+    scanf("%d %d", &arr2[i].t1, &arr2[i].t2);
+  }
+  // 오름차순 정렬
+  sort(arr2, arr2 + 10, comp1);
+  for(int i = 0; i < 10; i++) {
+    printf("arr2[%d] : %d %d\n", i, arr2[i].t1, arr2[i].t2);
+  }
+  // 내림차순 정렬
+  sort(arr2, arr2 + 10, comp2);
+  for(int i = 0; i < 10; i++) {
+    printf("arr2[%d] : %d %d\n", i, arr2[i].t1, arr2[i].t2);
+  }
+}
